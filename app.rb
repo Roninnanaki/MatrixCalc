9a2524d0d43b4478a5749c16f9a94df6cd125348
roninnanaki <roninnanaki@me.com>
2012-09-08 13:27:33 -0600

    testing 123

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+post 'create_matrix' do
+  "Hello World"
+end
+
+post "/mutliply" do
+  erb :result
+end
+
+post '/addition' do
+  erb :result
+end
+
+post '/inverse' do
+  erb :result
+end
+
+post '/transpose' do
+  erb :result
+end
---
+++ models/j_matrix.rb
@@ -0,0 +1,23 @@
+Class JMatrix
+  attr_reader :matrix
+
+  def initialize(matrix)
+    @matrix = matrix
+  end
+
+  def matrix_multiplication(other_matrix)
+
+  end
+
+  def matrix_addition(other_matrix)
+
+  end
+
+  def inverse 
+    
+  end
+
+  def transpose
+
+  end
+end
